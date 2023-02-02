04491bea703e64645373748e5e2b79a31a0acdd9
Chris Vickers <chris.vickers@grin.co>
2023-02-01 19:00:43 -0500

    Fixing issue with composer files

--- src/AssetCdnServiceProvider.php
+++ src/AssetCdnServiceProvider.php
@@ -5,43 +5,21 @@ namespace Arubacao\AssetCdn;
 use Arubacao\AssetCdn\Commands\EmptyCommand;
 use Arubacao\AssetCdn\Commands\PushCommand;
 use Arubacao\AssetCdn\Commands\SyncCommand;
-use Illuminate\Support\ServiceProvider;
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class AssetCdnServiceProvider extends ServiceProvider
+class AssetCdnServiceProvider extends PackageServiceProvider
 {
-    /**
-     * Bootstrap the application events.
-     *
-     * @return void
-     */
-    public function boot()
+
+    public function configurePackage(Package $package): void
     {
-        $this->publishes([
-            __DIR__.'/../config/asset-cdn.php' => config_path('asset-cdn.php'),
-        ], 'config');
+        $package->name('asset-cdn')
+            ->hasCommands([
+                PushCommand::class,
+                SyncCommand::class,
+                EmptyCommand::class
+            ])
+            ->hasConfigFile('asset-cdn');
     }
 
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->mergeConfigFrom(__DIR__.'/../config/asset-cdn.php', 'asset-cdn');
-
-        $this->app->singleton(Finder::class, function ($app) {
-            return new Finder(new Config($app->make('config'), $app->make('path.public')));
-        });
-
-        $this->app->bind('command.asset-cdn:push', PushCommand::class);
-        $this->app->bind('command.asset-cdn:sync', SyncCommand::class);
-        $this->app->bind('command.asset-cdn:empty', EmptyCommand::class);
-
-        $this->commands([
-            'command.asset-cdn:push',
-            'command.asset-cdn:sync',
-            'command.asset-cdn:empty',
-        ]);
-    }
 }
